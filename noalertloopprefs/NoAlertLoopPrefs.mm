14f944d98f9c8902b4abc7ce0591471e3e19b5c8
Jake <jake0oo0dev@gmail.com>
2015-08-18 19:44:17 -0500

    prefs

--- noalertloopprefs/NoAlertLoopPrefs.mm
+++ noalertloopprefs/NoAlertLoopPrefs.mm
@@ -11,6 +11,14 @@
 	}
 	return _specifiers;
 }
+
+- (void)openTwitter:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/itsjake88"]];
+}
+
+- (void)openGithub:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/Jake0oo0"]];
+}
 @end
 
 // vim:ft=objc
